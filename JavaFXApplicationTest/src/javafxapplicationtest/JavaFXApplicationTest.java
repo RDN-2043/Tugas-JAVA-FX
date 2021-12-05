ccc4044e8c849c1c05a6ac5def291cb87d0c575f
Rafi Dinata <66191277+TmXt@users.noreply.github.com>
2021-12-05 12:27:37 +0700

    Fix
    File setelah dimodifikasi

--- JavaFXApplicationTest/src/javafxapplicationtest/JavaFXApplicationTest.java
+++ JavaFXApplicationTest/src/javafxapplicationtest/JavaFXApplicationTest.java
@@ -33,18 +33,40 @@ public class JavaFXApplicationTest extends Application {
     
     @Override
     public void start(Stage primaryStage) {
-        Button btnOK = new Button();
-        Button btnCancel = new Button();
-        TextField tf = new TextField();
-        Label labelInput = new Label("Input Nama: ");
+        Button btnRegister = new Button("Register");
+
+        Label label_FirstName = new Label("First Name ");
+        Label label_LastName = new Label("Last Name ");
+        Label label_Email = new Label("E-Mail Address ");
+        Label label_Contact = new Label("Contact No ");
+        Label label_Password = new Label("Password ");
+        Label label_ConfirmPassword = new Label("Confirm Password ");
+
+        TextField tf_FirstName = new TextField();
+        TextField tf_LastName = new TextField();
+        TextField tf_Email = new TextField();
+        TextField tf_Contact = new TextField();
+        TextField tf_Password = new TextField();
+        TextField tf_ConfirmPassword = new TextField();
+
+        HBox hbox_FirstName = new HBox();
+        HBox hbox_LastName = new HBox();
+        HBox hbox_Email = new HBox();
+        HBox hbox_Contact = new HBox();
+        HBox hbox_Password = new HBox();
+        HBox hbox_ConfirmPassword = new HBox();
+        
+        VBox vbox_FieldHolder = new VBox();
+        
+        /*Button btnCancel = new Button();
         Label labelOutput = new Label("Output: ");
         Rectangle rect = new Rectangle(5,95,290,30);
         rect.setFill(Color.BURLYWOOD);
         
-        btnOK.setText("OK");
+        
         btnCancel.setText("Cancel");
         
-        btnOK.setOnAction(new EventHandler<ActionEvent>() {
+        btnOk.setOnAction(new EventHandler<ActionEvent>() {
             
             @Override
             public void handle(ActionEvent event) {
@@ -62,35 +84,52 @@ public class JavaFXApplicationTest extends Application {
         
         btnCancel.setOnMouseExited(event -> {
             System.out.println("Mouse Keluar");
-        });
+        });*/
         
+        hbox_FirstName.getChildren().addAll(label_FirstName, tf_FirstName);
+        hbox_FirstName.setSpacing(50);
+
+        hbox_LastName.getChildren().addAll(label_LastName, tf_LastName);
+        hbox_LastName.setSpacing(51);
+
+        hbox_Email.getChildren().addAll(label_Email, tf_Email);
+        hbox_Email.setSpacing(29);
+
+        hbox_Contact.getChildren().addAll(label_Contact, tf_Contact);
+        hbox_Contact.setSpacing(49);
+
+        hbox_Password.getChildren().addAll(label_Password, tf_Password);
+        hbox_Password.setSpacing(59);
         
-        Group root = new Group();
-        HBox panel1 = new HBox();
-        TilePane panel2 = new TilePane();
+        hbox_ConfirmPassword.getChildren().addAll(label_ConfirmPassword, tf_ConfirmPassword);
+        hbox_ConfirmPassword.setSpacing(14);
+
+        vbox_FieldHolder.getChildren().addAll(hbox_FirstName, hbox_LastName, hbox_Email, hbox_Contact, hbox_Password, hbox_ConfirmPassword);
+        vbox_FieldHolder.setSpacing(10);
+
+        vbox_FieldHolder.setLayoutX(30);
+        vbox_FieldHolder.setLayoutY(30);
+
+        Group group = new Group();
+        TilePane tp_Panel2 = new TilePane();
         
-        panel1.getChildren().add(labelInput);
-        panel1.getChildren().add(tf);
+        tp_Panel2.getChildren().addAll(btnRegister);
+        btnRegister.setStyle("-fx-background-color: #3d85c6; ");
         
-        panel2.getChildren().addAll(btnCancel,btnOK);
+        tp_Panel2.setLayoutX(150);
+        tp_Panel2.setLayoutY(250);
         
-        panel1.setLayoutX(10);
-        panel1.setLayoutY(10);
+        //labelOutput.setLayoutX(10);
+        //labelOutput.setLayoutY(100);
         
-        panel2.setLayoutX(100);
-        panel2.setLayoutY(50);
+        group.getChildren().addAll(vbox_FieldHolder, tp_Panel2);//,labelOutput);
         
-        labelOutput.setLayoutX(10);
-        labelOutput.setLayoutY(100);
+        Scene scene = new Scene(group, 350, 300);
+        scene.setFill(Color.WHITE);
+        //hbox_Panel1.setBackground(new Background(new BackgroundFill(Color.ANTIQUEWHITE,null,null)));
+        //hbox_Panel1.setPadding(new Insets(5,5,5,5));
         
-        root.getChildren().addAll(rect,panel1,panel2,labelOutput);
-        
-        Scene scene = new Scene(root, 300, 250);
-        scene.setFill(Color.DARKCYAN);
-        panel1.setBackground(new Background(new BackgroundFill(Color.ANTIQUEWHITE,null,null)));
-        panel1.setPadding(new Insets(5,5,5,5));
-        
-        primaryStage.setTitle("Test JavaFX");
+        primaryStage.setTitle("Registrasi");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
